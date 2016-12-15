c355313d91b21cb5776623323db18db6bd304a0f
Tony <phillyeagles60@gmail.com>
2016-12-14 19:39:29 -0800

    Fully fixed Allocation size

--- get_next_line.c
+++ get_next_line.c
@@ -13,7 +13,6 @@
 
 #include "get_next_line.h"
 #include <stdio.h>
-#include <string.h>
 
 //TODO still check if newline in leftover
 
@@ -25,14 +24,16 @@
 **
 ** @return 			the str with more memory allocated
 */
-char	*ft_strrealloc(char *str, size_t amount)
+char	*ft_strrealloc(t_ext *lst, char *str, size_t amount)
 {
 	char	*old;
 
-	old = ft_strnew(ft_strlen(str));
+	printf("Strlen is: %zu Amount is: %zu\n", ft_strlen(str), amount);
+	printf("Allocating size: %zu\n", BUFF_SIZE * (amount + 1) + ft_strlen(lst->leftover));
+	old = ft_strnew(ft_strlen(str) + ft_strlen(lst->leftover));
 	ft_strcpy(old, str);
-	str = ft_strnew(BUFF_SIZE * (amount + 1));
-	ft_bzero(str, BUFF_SIZE * (amount + 1));
+	str = ft_strnew(BUFF_SIZE * (amount + 1) + ft_strlen(lst->leftover));
+	ft_bzero(str, BUFF_SIZE * (amount + 1) + ft_strlen(lst->leftover));
 	ft_strncpy(str, old, BUFF_SIZE * (amount + 1) + 1);
 	free(old);
 	return (str);
@@ -68,13 +69,13 @@ int 	ft_read_data(t_ext *lst, char *data)
 	{
 		if (ft_strchr(data, '\n'))
 		{
-			lst->line = ft_strrealloc(lst->line, (lst->cur_place / BUFF_SIZE) + ft_strlen(lst->leftover));
+			lst->line = ft_strrealloc(lst, lst->line, (lst->cur_place / BUFF_SIZE));// + ft_strlen(lst->leftover));
 			lst->line = (ft_strncat(lst->line, data, ft_strchr(data, '\n') - data));
 			lst->leftover = ft_strdup(data + (ft_strchr(data, '\n') - data + 1));
 		}
 		else
 		{
-			lst->line = ft_strrealloc(lst->line, (lst->cur_place / BUFF_SIZE) + ft_strlen(lst->leftover));
+			lst->line = ft_strrealloc(lst, lst->line, (lst->cur_place / BUFF_SIZE));// + ft_strlen(lst->leftover));
 			lst->line = ft_strncat(lst->line, data, c_read);
 			ft_bzero(lst->leftover, ft_strlen(lst->leftover));		}
 	}
@@ -117,6 +118,7 @@ t_ext 	*iterate_lst_fd(t_ext *lst, int fd)
 	}
 	while (lst)
 	{
+		lst->cur_place = 0;
 		if (lst->fd == fd)
 		{
 			return (lst);
@@ -160,9 +162,9 @@ int		get_next_line(const int fd, char **line)
 		data = ft_strnew(ft_strlen(lst->line) + BUFF_SIZE);
 		ret = ft_read_data(lst, data);
 	}
-	if (ft_strchr(lst->line, '\n'))
+	if (ft_strchr(lst->leftover, '\n') && ret < BUFF_SIZE)
 	{
-		lst->line = ft_strndup(lst->line, ft_strchr(lst->line, '\n') - lst->line);
+		lst->line = ft_strndup(lst->leftover, ft_strchr(lst->leftover, '\n') - lst->leftover);
 		lst->leftover = ft_strdup(lst->leftover + (ft_strchr(lst->leftover, '\n') - lst->leftover + 1));
 		ret = 1;
 	}
