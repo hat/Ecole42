39d2f56144e31c1626e9b8e925d73b8b1b4babae
Tony <phillyeagles60@gmail.com>
2016-12-16 11:59:00 -0800

    All works but Stdin
    And the leaks are phenomenal

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -2,24 +2,36 @@
 #include <stdio.h>
 
 //TODO IN MAIN FUNCTION CHECK IF NEED TO READ OR GRAB FROM LEFTOVER ETC...
+//TODO CHECK REALLOC AND MAKE SURE TO SAVE BUFFER AFTER NEWLINE
 
+/*
+**
+**
+**
+*/
 char	*ft_realloc(char *str, size_t amount)
 {
-	char	*old;
+	char	*new;
 
-	if (str)
+	if (str[0])
 	{
-		old = ft_memalloc(ft_strlen(str));
-		ft_memcpy(old, str, ft_strlen(str));
-		ft_bzero(str, BUFF_SIZE * (amount + 1));
-		ft_memcpy(str, old, BUFF_SIZE * (amount + 1));
-		free(old);
+		new = ft_strnew(BUFF_SIZE * (amount + 1));
+		ft_strncpy(new, str, BUFF_SIZE * (amount + 1));
+		ft_bzero(str, ft_strlen(str));
 	}
 	else
-		str = ft_strnew(BUFF_SIZE * (amount + 1));
-	return (str);
+	{
+		new = ft_strnew(BUFF_SIZE * (amount + 1));
+		ft_bzero(new, BUFF_SIZE * (amount + 1));
+	}
+	return (new);
 }
 
+/*
+**
+**
+**
+*/
 t_list	*iterate_lst_fd(t_list *lst, int fd)
 {
 	if (lst->content_size == (size_t)fd)
@@ -38,50 +50,80 @@ t_list	*iterate_lst_fd(t_list *lst, int fd)
 	return (lst);
 }
 
+/*
+**
+**
+**
+*/
 ssize_t		ft_read_line(t_list *lst, char **line)
 {
 	ssize_t	b_read;
 
+	//printf("\nSTART ft_read_line\n");
 	b_read = 1;
 	if (!lst->content)
 	{
-		b_read = 0;
+		//printf("Null returning 0\n");
 		*line = NULL;
+		return (0);
 	}
 	else
 	{
+		*line = ft_realloc(lst->content, (ft_strlen(lst->content) / BUFF_SIZE));
+		//printf("Line before read: %s\n", *line);
+		if (ft_strchr(*line, '\n'))
+				return (0);
 		if (!(ft_strchr(lst->content, '\n')))
-		{
-			printf("Trying to read\n");
 			b_read = read(lst->content_size, lst->content, BUFF_SIZE);
-			printf("B_read = %zd\n", b_read);
-		}
+				if (b_read == -1)
+					return (-1);
+					else
+						//printf("Read: %s\n", lst->content);
 		if (ft_strchr(lst->content, '\n'))
 		{
-			*line = ft_realloc(*line, (ft_strlen(lst->content) / BUFF_SIZE));
+			//printf("1\n");
 			*line = (ft_strncat(*line, lst->content, ft_strchr(lst->content, '\n') - (char *)lst->content));
-			lst->content = ft_strdup(lst->content + (ft_strchr(lst->content, '\n') - (char *)lst->content + 1));
-			b_read = 0;
+			lst->content = ft_strdup(lst->content + (ft_strchr(lst->content, '\n') - (char *)lst->content) + 1);
+			//printf("1 Line: %s Content: %s\n", *line, lst->content);
+			return (0);
 		}
 		else
 		{
-			*line = ft_realloc(*line, (ft_strlen(lst->content) / BUFF_SIZE));
-			*line = (ft_strdup(lst->content));
-			lst->content = NULL;
+			//printf("Else...\n");
+			if (b_read)
+			{
+				//printf("2\n");
+				*line = ft_strcat(*line, lst->content);//(ft_strdup(lst->content));
+				//printf("Line is: %s\n", *line);
+				if (b_read < BUFF_SIZE)
+				{
+					lst->content = NULL;
+					return (0);
+				}
+			}
+			else
+			{
+				//printf("3\n");
+				lst->content = NULL;
+				return (0);
+			}
 		}
 	}
-	//printf("***ft_read_line lst->content returned: %s\n", lst->content);
-	//printf("***ft_read_line line returned: %s\n", *line);
-	printf("***ft_read_line return: %zd\n", b_read);
-	return (b_read);
+	return (ft_strlen(*line));
 }
 
+/*
+**
+**
+**
+*/
 int		get_next_line(const int fd, char **line)
 {
 	static t_list	*head;
 	t_list			*lst;
 	int				ret;
 
+	//printf("We're here, bitch!\n");
 	ret = 1;
 	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
@@ -93,7 +135,14 @@ int		get_next_line(const int fd, char **line)
 	{
 		ret = ft_read_line(lst, line);
 	}
-	if (*line && ret != -1)
+	if (*line && ft_strchr(*line, '\n'))
+	{
+		lst->content = ft_strdup(ft_strchr(*line, '\n') + 1);
+		*line = (ft_strndup(*line, ft_strchr(*line, '\n') - *line));
+	}
+	//printf("Line is: %s\n", *line);
+	//printf("Leftover is: %s\n", lst->content);
+	if (*line && ft_strlen(*line) && ret != -1)
 	{
 		return (1);
 	}
