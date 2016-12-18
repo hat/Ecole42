ec63643198b8be19e692e5fe1e08dfe33e3b5bda
Tony <phillyeagles60@gmail.com>
2016-12-17 23:31:33 -0800

    Updated fix for not stopping on

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,14 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thendric <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/17 21:56:53 by thendric          #+#    #+#             */
+/*   Updated: 2016/12/17 21:56:56 by thendric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "get_next_line.h"
 #include <stdio.h>
 
-//TODO Need case for two newlines in middle of file?
-
-/*
-**
-**
-**
-*/
-//TODO also add size of lst->content to realloc
 char	*ft_realloc(t_list *lst, char *str, size_t amount)
 {
 	char	*new;
@@ -25,40 +30,57 @@ char	*ft_realloc(t_list *lst, char *str, size_t amount)
 		ft_bzero(new, BUFF_SIZE * (amount + 1));
 	}
 	free(str);
-	//printf("Allocated to size: %zu\n", BUFF_SIZE * (amount + 1) + ft_strlen(lst->content));
 	return (new);
 }
 
-/*
-**
-**
-**
-*/
 t_list	*iterate_lst_fd(t_list *lst, int fd)
 {
-	//printf("Lst->content: %s\n", lst->content);
 	if (lst->content_size == (size_t)fd)
-	{
 		return (lst);
-	}
 	while (lst)
 	{
 		if (lst->content_size == (size_t)fd)
+			return (lst);
+		if (lst->next == NULL)
 		{
+			lst->next = ft_lstnew(ft_strnew(BUFF_SIZE), (size_t)fd);
+			lst = lst->next;
+			lst->content = ft_strnew(BUFF_SIZE);
+			ft_bzero(lst->content, ft_strlen(lst->content));
 			return (lst);
 		}
 		lst = lst->next;
 	}
-	lst = ft_lstnew(ft_strnew(BUFF_SIZE), (size_t)fd);
-	lst->content = ft_strnew(BUFF_SIZE);
 	return (lst);
 }
 
-/*
-**
-**
-**
-*/
+ssize_t		f_norm(t_list *lst, char **line, ssize_t b_read)
+{
+	if (ft_strchr(lst->content, '\n'))
+	{
+		*line = (ft_strncat(*line, lst->content, ft_strchr(lst->content, '\n')
+			- (char *)lst->content));
+		lst->content = ft_strdup(lst->content + (ft_strchr(lst->content, '\n')
+			- (char *)lst->content) + 1);
+		return (0);
+	}
+	if (b_read)
+	{
+		*line = ft_strcat(*line, lst->content);
+		if (b_read < BUFF_SIZE)
+		{
+			lst->content = NULL;
+			return (0);
+		}
+	}
+	else
+	{
+		lst->content = NULL;
+		return (0);
+	}
+	return (1);
+}
+
 ssize_t		ft_read_line(t_list *lst, char **line)
 {
 	ssize_t	b_read;
@@ -69,53 +91,24 @@ ssize_t		ft_read_line(t_list *lst, char **line)
 		*line = NULL;
 		return (0);
 	}
+	if (*line)
+		*line = ft_realloc(lst, *line, (ft_strlen(*line) / BUFF_SIZE));
 	else
+		*line = ft_realloc(lst, lst->content, (ft_strlen(lst->content) / BUFF_SIZE));
+	if (ft_strchr(*line, '\n'))
+		*line = ft_strdup(*line + (ft_strchr(*line, '\n') - *line) + 1);
+	if (!(ft_strchr(lst->content, '\n')))
 	{
-		if (*line)
-			*line = ft_realloc(lst, *line, (ft_strlen(*line) / BUFF_SIZE));
-		else
-			*line = ft_realloc(lst, lst->content, (ft_strlen(lst->content) / BUFF_SIZE));
-		if (ft_strchr(*line, '\n'))
-				return (0);
-		if (!(ft_strchr(lst->content, '\n')))
-		{
-			lst->content = ft_strnew(BUFF_SIZE);
-			b_read = read(lst->content_size, lst->content, BUFF_SIZE);
-			if (b_read == -1)
-				return (-1);
-		}
-		if (ft_strchr(lst->content, '\n'))
-		{
-			*line = (ft_strncat(*line, lst->content, ft_strchr(lst->content, '\n') - (char *)lst->content));
-			lst->content = ft_strdup(lst->content + (ft_strchr(lst->content, '\n') - (char *)lst->content) + 1);
-			return (0);
-		}
-		else
-		{
-			if (b_read)
-			{
-				*line = ft_strcat(*line, lst->content);
-				if (b_read < BUFF_SIZE)
-				{
-					lst->content = NULL;
-					return (0);
-				}
-			}
-			else
-			{
-				lst->content = NULL;
-				return (0);
-			}
-		}
+		lst->content = ft_strnew(BUFF_SIZE);
+		b_read = read(lst->content_size, lst->content, BUFF_SIZE);
+		if (b_read == -1)
+			return (-1);
 	}
+	if (!f_norm(lst, line, b_read))
+		return (0);
 	return (ft_strlen(*line));
 }
 
-/*
-**
-**
-**
-*/
 int		get_next_line(const int fd, char **line)
 {
 	static t_list	*head;
@@ -131,13 +124,10 @@ int		get_next_line(const int fd, char **line)
 		head->content = ft_strnew(BUFF_SIZE);
 	lst = head;
 	lst = iterate_lst_fd(lst, fd);
-	//For the 1 buff case
 	if (*line)
 		*line = NULL;
-	while (ret && ret != -1)
-	{
+	while (ret && ret != -1 && ret != -2)
 		ret = ft_read_line(lst, line);
-	}
 	if (*line && ft_strchr(*line, '\n'))
 	{
 		lst->content = ft_strdup(ft_strchr(*line, '\n') + 1);
