1479a7e34e8b8d946511e6792badde84185c3b3e
Tony <phillyeagles60@gmail.com>
2016-12-20 12:15:15 -0800

    Add files via upload

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,31 +6,32 @@
 /*   By: thendric <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/29 11:33:27 by thendric          #+#    #+#             */
-/*   Updated: 2016/12/10 13:57:24 by thendric         ###   ########.fr       */
+/*   Updated: 2016/12/19 15:59:12 by thendric         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 12
+# define BUFF_SIZE 5
 
 # include <stdlib.h>
 # include <unistd.h>
-# include "../libft/libft.h"
+# include "libft/libft.h"
 
 /*
 ** Struct to hold the line read from multiple
 ** file descriptors
 ** fd:		file descriptor
-** *line: 	text read from file
-** *next:	pointer to the next struct
+** line: 	text read from file
+** next:	pointer to the next struct
 */
+
 typedef struct			s_readfile
 {
 	int					fd;
 	char				*line;
-	char				*leftover;
+	char				*buf;
 	size_t				cur_place;
 	struct s_readfile	*next;
 
@@ -39,6 +40,7 @@ typedef struct			s_readfile
 /*
 ** Declare all functions
 */
+
 int						get_next_line(const int fd, char **line);
 
 #endif
