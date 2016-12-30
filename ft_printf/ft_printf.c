ef1d6821c67c84b0d1161dc2ade35a3912b4c30a
Tony <phillyeagles60@gmail.com>
2016-12-29 22:32:15 -0800

    Added more conversions x,p,o and started # flag

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -56,6 +56,21 @@ int 	ft_convers_id(t_input *input)
 	return (0);
 }
 
+
+//NOT WORKING
+int 	ft_convers_u(t_input *input)
+{
+	unsigned int 	num;
+	char 	*numstr;
+
+	num  = (unsigned int)va_arg(input->ap, int);
+	numstr = ft_itoa(num);
+	input->str = ft_strjoin(input->str, numstr);
+	//1 will be number of flags!
+	input->form = input->form + 1;
+	return (0);
+}
+
 int 	ft_convers_f(t_input *input)
 {
 	double 	num;
@@ -66,6 +81,61 @@ int 	ft_convers_f(t_input *input)
 	//Create print number with decimal point
 	//ft_putnbr(num);
 	return (0);
+}
+
+//BOTH BELOW NEED FIX FOR # FLAG
+//IF FORM + 2 THEN AP GETS MESSED UP IF CALLED TWICE
+//BUT WORKS IF CALLED ONCE...???
+
+int 	ft_convers_oO(t_input *input)
+{
+	int 	flag;
+	int 	num;
+	char 	*numstr;
+
+	flag = 1;
+	num  = va_arg(input->ap, int);
+	//printf("Num: %d\n", num);
+	// if (num < 0)
+	// 	num -= -2147483648;
+	numstr = ft_itoa_base(num, 8);
+	if (input->form[1] == '#')
+	{
+		input->str = ft_strjoin(input->str, "0");
+		input->form += 1;
+	}
+	input->str = ft_strjoin(input->str, numstr);
+	input->form += 1;
+	return (0);
+}
+
+int 	ft_convers_xX(t_input *input)
+{
+	int 	num;
+	char 	*numstr;
+
+	num  = va_arg(input->ap, int);
+	numstr = ft_itoa_base(num, 16);
+	if (input->form[1] == '#')
+		input->str = ft_strjoin(input->str, "0x");
+	if (numstr && input->c == 'X')
+		ft_touppercase(numstr);
+	input->str = ft_strjoin(input->str, numstr);
+	input->form += 1;
+	return (0);
+}
+
+int 	ft_convers_p(t_input *input)
+{
+	int num;
+	char *numstr;
+
+	num = va_arg(input->ap, int);
+	numstr = ft_itoa_base(num, 16);
+	input->str = ft_strjoin(input->str, "0x10");
+	input->str = ft_strjoin(input->str, numstr);
+	input->form += 1;
+	return(0);
 }
 
 int 	ft_pickconvers(t_input *input)
@@ -80,6 +150,14 @@ int 	ft_pickconvers(t_input *input)
 		ft_convers_f(input);
 	if (input->c == '%')
 		ft_convers_percent(input);
+	if (input->c == 'o' || input->c == 'O')
+		ft_convers_oO(input);
+	if (input->c == 'x' || input->c == 'X')
+		ft_convers_xX(input);
+	if (input->c == 'u')
+		ft_convers_u(input);
+	if (input->c == 'p')
+		ft_convers_p(input);
 	return (0);
 }
 
@@ -100,10 +178,10 @@ int 	ft_findconvers(t_input *input)
 	{
 		c = input->form[i + 1];
 		if (c == 'd' || c == 'i' || c == 's' || c == 'c'
-			|| c == '%' || c == 'f')
+			|| c == '%' || c == 'f' || c == 'o' || c == 'x'
+			|| c == 'u' || c == 'O' || c == 'X' || c == 'p')
 		{
 			input->c = c;
-			input->form = input->form + i;
 			ft_pickconvers(input);
 		}
 		i++;
@@ -115,11 +193,12 @@ int 	ft_findconvers(t_input *input)
 
 int 	ft_percentsign(t_input *input)
 {
+	int 	flags;
 	char 	*prev;
 
+	flags = 2;
 	prev = ft_strndup(input->form, (ft_strchr(input->form, '%') - input->form));
 	input->form = input->form + (ft_strchr(input->form, '%') - input->form);
-	//printf("IF: %s\n", input->form);
 	if (!input->str)
 		input->str = ft_strdup(prev);
 	else
