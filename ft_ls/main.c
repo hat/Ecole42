48d147ae644b159c71a631d00c93be0b364970f2
Tony Hendrick <thendric@Tonys-iMac.local>
2017-03-28 14:06:43 -0700

    l r a flags all work simultaneously

--- ft_ls/main.c
+++ ft_ls/main.c
@@ -13,6 +13,8 @@
 
 #include <math.h> // Add to libft
 
+//ADD THE 
+
 char	*ft_substr(char *str, int pos, int len)
 {
 	char	*new_str;
@@ -46,11 +48,16 @@ void	print_dirs_long(t_all *lst, char *file, int i)
 	struct group	*gr;
 	char			*date;
 
+
+
 	path = ft_strnew(ft_strlen(lst->dirv[i]) + 1);
 	path = ft_strjoin(lst->dirv[i], "/");
 	path = ft_strjoin(path, file);
     if(lstat(path, &fileStat) < 0)
+    {
+    	ft_printf("ERROR: Could not get stats of: %s\n", path);
         return ;
+    }
     date = ft_strnew(10);
     //FILE PERMISSIONS
     ft_printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-"); // can possibly be 'l'
@@ -81,16 +88,16 @@ void	print_dirs_long(t_all *lst, char *file, int i)
     ft_strdel(&date);
 }
 
-void	print_dirs_reverse(t_all *lst, t_file *file, int i)
+void	print_dirs_reverse(t_all *lst, t_file *file, int i, int nl)
 {
 	if (!file)
 		return ;
-	if (lst->dirc > 1 && i != -1)
+	if (lst->dirc > 1 && nl != -1)
 	{
 		ft_printf("%s:\n", lst->dirv[i]);
-		i = -1;
+		nl = -1;
 	}
-	print_dirs_reverse(lst, file->next, i);
+	print_dirs_reverse(lst, file->next, i, nl);
 	if (file->name)
 	{
 		if (check_bit(lst->options, 2))
@@ -117,7 +124,7 @@ void	print_dirs(t_all *lst, t_file *file, int i)
 void	check_print_dirs(t_all *lst, t_file *file, int i)
 {
 	if ( check_bit(lst->options, 4) )
-		print_dirs_reverse(lst, file, i);
+		print_dirs_reverse(lst, file, i, 1);
 	// else if ( check_bit(lst->options, 3) )
 	// 	print_dirs_recursively(lst);
 	else
@@ -198,7 +205,7 @@ void	get_dir(t_all *lst, int argc, char *argv[])
 		ft_strdel(&path);
 		i++;
 	}
-	lst->dirc = (lst->dirc == 2) ? 1 : lst->dirc - 1;
+	lst->dirc = (lst->dirc == 1) ? 1 : lst->dirc - 1;
 }
 
 void	get_options(t_all *lst, int argc, char *argv[])
@@ -247,7 +254,6 @@ int		main(int argc, char *argv[])
 	get_dir(lst, argc, argv);
 	if (argc > 1)
 		get_options(lst, argc, argv);
-	ft_printf("Dir count: %d\n", lst->dirc);
 	while (cur_dir < lst->dirc)
 	{
 		file = get_all(lst, cur_dir);
